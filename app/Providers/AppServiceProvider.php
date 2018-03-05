0b004712f71b1f3c3221eca86c9c431718820f34
Maarten Kuiper <maarten@dtcmedia.nl>
2018-03-05 21:56:00 +0100

    Laravel startup with auth scaffold and fix for MySQL 5.6.x

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // fix for MySQL 5.6.x
+        // https://hdtuto.com/article/laravel-55-1071-specified-key-was-too-long-max-key-length-is-767-bytes
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
